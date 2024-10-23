e321208e7692c7ab479df89c5a188da84bcfeb33
Jordan Packer <jordanpacker@jordans-mbp-2.wifi.nic.edu>
2024-10-23 10:21:48 -0700

    please

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream> 
 using namespace std;
-
+void say(string word);
 
 int main(){
 
     cout<<"hello World\n";
-
-
+    cout<<"5 + 7 =";
+    say("hi");
     return 0;
+}
+
+void say(string word)
+{
+
+    cout<<word<<endl;
 }

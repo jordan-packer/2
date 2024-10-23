22a8e6ea8da18fa1907de74b2d7fa493b62bdbe7
Jordan Packer <jordanpacker@jordans-mbp-2.wifi.nic.edu>
2024-10-23 10:27:54 -0700

    A

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream> 
 using namespace std;
 void say(string word);
-
+int add(int a, int b);
 int main(){
 
     cout<<"hello World\n";
-    cout<<"5 + 7 =";
+    cout<<"5 + 7 = "<<add(5,7)<<endl;
     say("hi");
     return 0;
 }
-
+int add (int a, int b)
+{
+    return a+b;
+}
 void say(string word)
 {
 
